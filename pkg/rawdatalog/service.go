cbe44b0f673ecb4047c1b0b1b74e14880acf5be3
freshteapot <chris@dolittle.com>
2021-06-10 11:55:55 +0200

    supporting allowed uriSuffix

--- pkg/rawdatalog/service.go
+++ pkg/rawdatalog/service.go
@@ -7,18 +7,20 @@ import (
 	"strings"
 	"time"
 
+	"github.com/dolittle-entropy/platform-api/pkg/platform"
 	"github.com/dolittle-entropy/platform-api/pkg/utils"
 	"github.com/sirupsen/logrus"
 )
 
 type service struct {
-	logContext    logrus.FieldLogger
-	repo          Repo
-	uriPrefix     string
-	topic         string
-	tenantID      string
-	applicationID string
-	environment   string
+	logContext         logrus.FieldLogger
+	repo               Repo
+	uriPrefix          string
+	topic              string
+	tenantID           string
+	applicationID      string
+	environment        string
+	allowedUriSuffixes map[string]platform.RawDataLogIngestorWebhookConfig
 }
 
 func NewService(logContext logrus.FieldLogger, uriPrefix string, topic string, repo Repo, tenantID string, applicationID string, environment string) service {
@@ -29,6 +31,13 @@ func NewService(logContext logrus.FieldLogger, uriPrefix string, topic string, r
 		tenantID:      tenantID,
 		applicationID: applicationID,
 		environment:   environment,
+		allowedUriSuffixes: map[string]platform.RawDataLogIngestorWebhookConfig{
+			"abc/abc": {
+				Kind:          "abc/abc",
+				UriSuffix:     "abc/abc",
+				Authorization: "Bearer Test",
+			},
+		},
 	}
 }
 
@@ -39,18 +48,46 @@ func (s *service) Webhook(w http.ResponseWriter, r *http.Request) {
 	environment := s.environment
 
 	pathname := r.URL.Path
+	pathname = strings.ToLower(pathname)
 	pathname = strings.TrimPrefix(pathname, s.uriPrefix)
 	pathname = strings.TrimPrefix(pathname, "/")
 	pathname = strings.TrimSuffix(pathname, "/")
+
+	// TODO read from config-files
+	webhook, ok := s.allowedUriSuffixes[pathname]
+	if !ok {
+		s.logContext.WithFields(logrus.Fields{
+			"error":   fmt.Sprintf("uriSuffix not on the list: %s", pathname),
+			"context": "verify webhook configured",
+		}).Error("webhook")
+		utils.RespondWithError(w, http.StatusForbidden, "Webhook not supported due to unknown uri suffix")
+		return
+	}
+
+	if r.Header.Get("Authorization") != webhook.Authorization {
+		s.logContext.WithFields(logrus.Fields{
+			"error":                "authorization failed",
+			"headerAuthorization":  r.Header.Get("Authorization"),
+			"webhookAuthorization": webhook.Authorization,
+			"webhookUriSuffix":     webhook.UriSuffix,
+			"context":              "checking authorization",
+		}).Error("webhook")
+		utils.RespondWithError(w, http.StatusForbidden, "Webhook not supported, failed authorization")
+		return
+	}
+
 	parts := strings.Split(pathname, "/")
 
-	labels := map[string]string{}
+	labels := map[string]string{
+		"uriSuffix": pathname,
+	}
+
 	for index, part := range parts {
 		indexStr := fmt.Sprintf("uri-%d", index)
 		labels[indexStr] = part
 	}
 
-	kind := "TODO"
+	kind := webhook.Kind
 	metadata := RawMomentMetadata{
 		TenantID:      tenantID,
 		ApplicationID: applicationID,
