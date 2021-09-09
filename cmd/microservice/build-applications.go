b71e7bd0c290678b8687adf8618fd9bcc75fa6f7
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-09-10 00:15:30 +0200

    Feature complete building of applications

--- cmd/microservice/build-applications.go
+++ cmd/microservice/build-applications.go
@@ -2,7 +2,10 @@ package microservice
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/dolittle-entropy/platform-api/pkg/platform"
 	"github.com/dolittle-entropy/platform-api/pkg/platform/storage"
@@ -12,7 +15,9 @@ import (
 	"github.com/thoas/go-funk"
 	appsV1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	netV1 "k8s.io/api/networking/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -59,6 +64,7 @@ var buildApplicationsCMD = &cobra.Command{
 	},
 }
 
+// SaveApplications saves the Applications
 func SaveApplications(repo storage.Repo, applications []platform.HttpResponseApplication) error {
 	for _, application := range applications {
 		studioConfig, _ := repo.GetStudioConfig(application.TenantID)
@@ -80,22 +86,26 @@ func SaveApplications(repo storage.Repo, applications []platform.HttpResponseApp
 
 func extractApplications(ctx context.Context, client *kubernetes.Clientset) []platform.HttpResponseApplication {
 	applications := make([]platform.HttpResponseApplication, 0)
-	namespaces, err := client.CoreV1().Namespaces().List(ctx, metaV1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
 
-	for _, ns := range namespaces.Items {
-		// Unique the environments
-		applications = append(applications, createApplicationFromK8s(ctx, client, ns))
+	for _, ns := range getNamespaces(ctx, client) {
+		// Creates a single application for a namespace(?)
+		applications = append(applications, getApplicationFromK8s(ctx, client, ns))
 	}
 
 	return applications
 }
 
-func createApplicationFromK8s(ctx context.Context, client *kubernetes.Clientset, namespace v1.Namespace) platform.HttpResponseApplication {
+func getNamespaces(ctx context.Context, client *kubernetes.Clientset) []v1.Namespace {
+	namespacesList, err := client.CoreV1().Namespaces().List(ctx, metaV1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return namespacesList.Items
+}
+
+func getApplicationFromK8s(ctx context.Context, client *kubernetes.Clientset, namespace v1.Namespace) platform.HttpResponseApplication {
 	application := createBasicApplicationStructureFromMetadata(client, namespace)
-	addEnvironmentsFromK8sInto(&application, ctx, client, namespace, application.TenantID, application.ID)
+	addEnvironmentsFromK8sInto(&application, ctx, client, namespace.GetObjectMeta().GetName())
 	return application
 }
 
@@ -114,18 +124,14 @@ func createBasicApplicationStructureFromMetadata(client *kubernetes.Clientset, n
 	}
 }
 
-func addEnvironmentsFromK8sInto(application *platform.HttpResponseApplication, ctx context.Context, client *kubernetes.Clientset, namespace v1.Namespace, tenantID, applicationID string) {
-	namespaceName := namespace.GetObjectMeta().GetName()
+func addEnvironmentsFromK8sInto(application *platform.HttpResponseApplication, ctx context.Context, client *kubernetes.Clientset, namespaceName string) {
 	for _, deployment := range getDeployments(ctx, client, namespaceName) {
 		if !deploymentHasReqiredMetadata(deployment) {
 			continue
 		}
 
-		environment := createEnvironmentFromK8s(deployment, tenantID, applicationID)
-
-		if environmentAlreadyInApplication(application.Environments, environment.Name) {
-			continue
-		}
+		environment := createOrGetEnvironment(ctx, client, deployment, namespaceName, *application)
+		addIngressesIntoEnvironment(&environment, ctx, client, namespaceName, deployment)
 
 		application.Environments = append(application.Environments, environment)
 	}
@@ -139,13 +145,83 @@ func getDeployments(ctx context.Context, client *kubernetes.Clientset, namespace
 	return deploymentList.Items
 }
 
-func createEnvironmentFromK8s(deployment appsV1.Deployment, tenantID, applicationID string) platform.HttpInputEnvironment {
+func createOrGetEnvironment(ctx context.Context, client *kubernetes.Clientset, deployment appsV1.Deployment, namespace string, application platform.HttpResponseApplication) platform.HttpInputEnvironment {
+	environmentName := deployment.ObjectMeta.Labels["environment"]
+	environmentExists, i := environmentAlreadyInApplication(application.Environments, environmentName)
+	if environmentExists {
+		return application.Environments[i]
+	}
 	return platform.HttpInputEnvironment{
-		Name:          deployment.ObjectMeta.Labels["environment"],
-		TenantID:      tenantID,
-		ApplicationID: applicationID,
+		Name:          environmentName,
+		TenantID:      application.TenantID,
+		ApplicationID: application.ID,
+		Tenants:       getTenants(ctx, client, namespace, environmentName),
+		Ingresses:     make(map[platform.TenantId]platform.EnvironmentIngress),
 	}
 }
+
+func getTenants(ctx context.Context, client *kubernetes.Clientset, namespace, environmentName string) []platform.TenantId {
+	tenants := make([]platform.TenantId, 0)
+
+	for tenantStr, _ := range getTenantsFromConfigmap(ctx, client, namespace, environmentName) {
+		tenants = append(tenants, platform.TenantId(tenantStr))
+	}
+	return tenants
+}
+
+func addIngressesIntoEnvironment(environment *platform.HttpInputEnvironment, ctx context.Context, client *kubernetes.Clientset, namespace string, deployment appsV1.Deployment) map[platform.TenantId]platform.EnvironmentIngress {
+	ingresses := environment.Ingresses
+	for _, ingress := range getIngresses(ctx, client, namespace, deployment) {
+		if len(ingress.Spec.TLS) > 0 {
+			// Assume that there is only one Rule, or that only the top one counts
+			// Note that this will override ingresses for tenants, not a problem if they are the same, but confusing if there are different configs
+			ingresses[getTenantFromIngress(ingress)] = extractEnvironmentIngressFromIngressRule(ingress.Spec.Rules[0])
+		}
+	}
+	return ingresses
+}
+
+func extractEnvironmentIngressFromIngressRule(rule netV1.IngressRule) platform.EnvironmentIngress {
+	host := rule.Host
+	domainPrefix := strings.ReplaceAll(host, ".dolittle.cloud", "")
+
+	// Note that this will override ingresses for tenants, not a problem if they are the same, but confusing if there are different configs
+	return platform.EnvironmentIngress{
+		Host:         host,
+		DomainPrefix: domainPrefix,
+	}
+}
+func getTenantFromIngress(ingress netV1.Ingress) platform.TenantId {
+	tenantHeaderAnnotation := ingress.GetObjectMeta().GetAnnotations()["nginx.ingress.kubernetes.io/configuration-snippet"]
+	tenantID := strings.ReplaceAll(tenantHeaderAnnotation, "proxy_set_header Tenant-ID", "")
+	tenantID = strings.ReplaceAll(tenantID, "\"", "")
+	return platform.TenantId(strings.TrimSpace(tenantID))
+}
+
+func getIngresses(ctx context.Context, client *kubernetes.Clientset, namespace string, deployment appsV1.Deployment) []netV1.Ingress {
+	ingresses, err := client.NetworkingV1().Ingresses(namespace).List(ctx, metaV1.ListOptions{
+		LabelSelector: labels.FormatLabels(deployment.Labels),
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	return ingresses.Items
+}
+
+func getTenantsFromConfigmap(ctx context.Context, client *kubernetes.Clientset, namespace, environmentName string) map[string]interface{} {
+	tenantsConfig, err := client.CoreV1().ConfigMaps(namespace).Get(ctx, fmt.Sprintf("%s-tenants", strings.ToLower(environmentName)), metaV1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	tenantsJson := tenantsConfig.Data["tenants.json"]
+	tenants := make(map[string]interface{})
+	err = json.Unmarshal([]byte(tenantsJson), &tenants)
+	if err != nil {
+		panic(err.Error())
+	}
+	return tenants
+}
+
 func deploymentHasReqiredMetadata(deployment appsV1.Deployment) bool {
 	_, ok := deployment.ObjectMeta.Annotations["dolittle.io/tenant-id"]
 	if !ok {
@@ -163,16 +239,18 @@ func deploymentHasReqiredMetadata(deployment appsV1.Deployment) bool {
 	}
 	return true
 }
-func environmentAlreadyInApplication(environments []platform.HttpInputEnvironment, environmentName string) bool {
+
+func environmentAlreadyInApplication(environments []platform.HttpInputEnvironment, environmentName string) (bool, int) {
 	index := funk.IndexOf(environments, func(item platform.HttpInputEnvironment) bool {
 		return item.Name == environmentName
 	})
 
 	if index != -1 {
-		return false
+		return false, index
 	}
-	return true
+	return true, index
 }
+
 func init() {
 	RootCmd.AddCommand(buildApplicationsCMD)
 	buildApplicationsCMD.Flags().String("kube-config", "", "FullPath to kubeconfig")
