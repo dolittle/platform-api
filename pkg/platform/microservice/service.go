7c6e3c3f69cbacb2354af0d788c7747ee2a45c2b
freshteapot <chris@dolittle.com>
2021-11-05 13:00:12 +0100

    Not json + first need to lookup service account

--- pkg/platform/microservice/service.go
+++ pkg/platform/microservice/service.go
@@ -558,7 +558,7 @@ func (s *service) GetSecret(w http.ResponseWriter, r *http.Request) {
 
 	secret, err := s.k8sDolittleRepo.GetSecret(logContext, applicationID, secretName)
 	if err != nil {
-		if err == platform.SecretNotFound {
+		if err == platform.NotFound {
 			utils.RespondWithError(w, http.StatusNotFound, fmt.Sprintf("Secret %s not found in application %s", secretName, applicationID))
 			return
 		}
