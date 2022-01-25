50711c12c7aeff741e22da70b4ee14dcdc1924b0
freshteapot <chris@dolittle.com>
2022-01-25 12:21:01 +0100

    fixup: after the move, missing studio under tools

--- cmd/tools/root.go
+++ cmd/tools/root.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"github.com/dolittle/platform-api/cmd/tools/studio"
 	"github.com/spf13/cobra"
 )
 
@@ -13,5 +14,6 @@ Tools to interact with the platform`,
 }
 
 func init() {
+	RootCmd.AddCommand(studio.RootCmd)
 	RootCmd.AddCommand(createCustomerHclCMD)
 }
