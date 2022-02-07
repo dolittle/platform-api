41c099323bda6e16f59f8557534a177a7bc74be2
freshteapot <chris@dolittle.com>
2022-02-07 10:36:39 +0100

    start at 0

--- pkg/platform/application/cron.go
+++ pkg/platform/application/cron.go
@@ -8,7 +8,7 @@ import (
 func GetRandomMinutes() int {
 	rand.Seed(time.Now().Unix())
 
-	rangeLower := 1
+	rangeLower := 0
 	rangeUpper := 59
 	randomNum := rangeLower + rand.Intn(rangeUpper-rangeLower+1)
 	return randomNum
