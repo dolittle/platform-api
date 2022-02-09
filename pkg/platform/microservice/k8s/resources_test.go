612cba88f3e3b099d6d259a7f147e8a11f3945da
freshteapot <chris@dolittle.com>
2022-02-09 10:58:08 +0100

    confirming the rules for a new microservice are correct

--- pkg/platform/microservice/k8s/resources_test.go
+++ pkg/platform/microservice/k8s/resources_test.go
@@ -1,12 +1,30 @@
 package k8s_test
 
 import (
+	"github.com/dolittle/platform-api/pkg/platform/microservice/k8s"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Microservice Resources", func() {
-	It("NewMicroservicePolicyRoles", func() {
-		Expect("TODO").To(Equal("TODO"))
+	It("NewMicroservicePolicyRules", func() {
+
+		name := "Order"
+		environment := "Dev"
+		resource := k8s.NewMicroservicePolicyRules(name, environment)
+
+		Expect(resource[0].Verbs).To(Equal([]string{
+			"get",
+			"patch",
+		}), "Confirm verbs")
+
+		Expect(resource[0].ResourceNames).To(Equal([]string{
+			"dev-order-env-variables",
+			"dev-order-config-files",
+		}), "Confirm configmaps")
+
+		Expect(resource[1].ResourceNames).To(Equal([]string{
+			"dev-order-secret-env-variables",
+		}), "Confirm secrets")
 	})
 })
