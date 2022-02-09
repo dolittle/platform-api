1ca98247ba0a34be4bf1aa68c811a96cb1fb1da4
freshteapot <chris@dolittle.com>
2022-02-09 12:49:12 +0100

    build-application-info allows for all

--- cmd/tools/studio/build-application-info.go
+++ cmd/tools/studio/build-application-info.go
@@ -1,17 +1,22 @@
 package studio
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/dolittle/platform-api/pkg/git"
 	"github.com/dolittle/platform-api/pkg/k8s"
+	"github.com/dolittle/platform-api/pkg/platform/automate"
 	platformK8s "github.com/dolittle/platform-api/pkg/platform/k8s"
 	"github.com/dolittle/platform-api/pkg/platform/manual"
+	"github.com/dolittle/platform-api/pkg/platform/storage"
 	gitStorage "github.com/dolittle/platform-api/pkg/platform/storage/git"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var buildApplicationInfoCMD = &cobra.Command{
@@ -31,10 +36,10 @@ var buildApplicationInfoCMD = &cobra.Command{
 		logrus.SetOutput(os.Stdout)
 		logger := logrus.StandardLogger()
 		dryRun, _ := cmd.Flags().GetBool("dry-run")
-		namespace := args[0]
+		resetAll, _ := cmd.Flags().GetBool("all")
+
 		logContext := logger.WithFields(logrus.Fields{
-			"cmd":       "build-application-info",
-			"namespace": namespace,
+			"cmd": "build-application-info",
 		})
 
 		platformEnvironment, _ := cmd.Flags().GetString("platform-environment")
@@ -49,82 +54,138 @@ var buildApplicationInfoCMD = &cobra.Command{
 		k8sRepoV2 := k8s.NewRepo(k8sClient, logger.WithField("context", "k8s-repo-v2"))
 		manualRepo := manual.NewManualHelper(k8sClient, k8sRepoV2, storageRepo, logContext.WithField("context", "manual-repo"))
 
-		application, err := manualRepo.GatherOne(platformEnvironment, namespace)
-
-		if err != nil {
-			logContext.WithFields(logrus.Fields{
-				"error": err,
-			}).Error("Failed to find namespace")
-			return
+		oneNamespace := ""
+		if len(args) > 0 {
+			oneNamespace = args[0]
 		}
 
-		logContext = logContext.WithFields(logrus.Fields{
-			"application_id": application.ID,
-		})
-
-		customer, err := storageRepo.GetTerraformTenant(application.TenantID)
-		if err != nil {
-			logContext.WithFields(logrus.Fields{
-				"error":       err,
-				"customer_id": application.TenantID,
-				"tip": fmt.Sprintf(
-					"didn't find a studio config for customer %s, create a config for this customer by running 'tools studio build-studio-info %s'",
-					application.TenantID,
-					application.TenantID,
-				),
-			}).Error("Failed to find customer terraform")
-			return
-
+		if oneNamespace != "" && resetAll {
+			logContext.Fatal("specify either a namespace or '--all' flag")
 		}
 
-		if customer.PlatformEnvironment != platformEnvironment {
-			logContext.WithFields(logrus.Fields{
-				"error":                         "platform-environment",
-				"customer_platform_environment": customer.PlatformEnvironment,
-				"platform_environment":          platformEnvironment,
-			}).Warn("Skipping")
-			return
+		var resources []corev1.Namespace
+		if resetAll {
+			logContext.Info("Discovering all namespaces with an application in the platform")
+			resources, _ = k8sRepoV2.GetNamespacesWithApplication()
 		}
 
-		studioConfig, err := storageRepo.GetStudioConfig(application.TenantID)
-		if err != nil {
-			logContext.WithFields(logrus.Fields{
-				"error":       err,
-				"customer_id": application.TenantID,
-				"tip": fmt.Sprintf(
-					"didn't find a studio config for customer %s, create a config for this customer by running 'tools studio build-studio-info %s'",
-					application.TenantID,
-					application.TenantID,
-				),
-			}).Error("Failed to find studio config")
-			return
+		if oneNamespace != "" {
+
+			ctx := context.TODO()
+			resource, err := k8sClient.CoreV1().Namespaces().Get(ctx, oneNamespace, metav1.GetOptions{})
+			fmt.Println(err)
+			resources = append(resources, *resource)
 		}
 
-		if !studioConfig.BuildOverwrite {
-			logContext.WithFields(logrus.Fields{
-				"error":       "build-overwrite-set",
-				"customer_id": application.TenantID,
-			}).Warn("Skipping")
-			return
+		if len(resources) == 0 {
+			logContext.Fatal("No namespaces found")
 		}
 
-		if dryRun {
-			b, _ := json.Marshal(application)
-			fmt.Println(string(b))
-			return
+		for _, resource := range resources {
+			if !automate.IsApplicationNamespace(resource) {
+				continue
+			}
+
+			namespace := resource.Name
+			customerID := resource.Annotations["dolittle.io/tenant-id"]
+			applicationID := resource.Annotations["dolittle.io/application-id"]
+			logContext := logger.WithFields(logrus.Fields{
+				"customer_id":    customerID,
+				"application_id": applicationID,
+			})
+
+			processOne(storageRepo, platformEnvironment, manualRepo, namespace, dryRun, logContext)
 		}
 
-		err = storageRepo.SaveApplication(application)
-		if err != nil {
-			logContext.WithFields(logrus.Fields{
-				"error":     err,
-				"namespace": namespace,
-			}).Fatal("Failed to write applicaiton")
-		}
 	},
+}
+
+func processOne(
+	storageRepo storage.Repo,
+	platformEnvironment string,
+	manualRepo manual.Repo,
+	namespace string,
+	dryRun bool,
+	logContext logrus.FieldLogger,
+) {
+
+	// TODO having this use all, will make it as simple as the others
+	application, err := manualRepo.GatherOne(platformEnvironment, namespace)
+
+	if err != nil {
+		logContext.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Failed to find namespace")
+		return
+	}
+
+	logContext = logContext.WithFields(logrus.Fields{
+		"application_id": application.ID,
+	})
+
+	customer, err := storageRepo.GetTerraformTenant(application.TenantID)
+	if err != nil {
+		logContext.WithFields(logrus.Fields{
+			"error":       err,
+			"customer_id": application.TenantID,
+			"tip": fmt.Sprintf(
+				"didn't find a studio config for customer %s, create a config for this customer by running 'tools studio build-studio-info %s'",
+				application.TenantID,
+				application.TenantID,
+			),
+		}).Error("Failed to find customer terraform")
+		return
+
+	}
+
+	if customer.PlatformEnvironment != platformEnvironment {
+		logContext.WithFields(logrus.Fields{
+			"error":                         "platform-environment",
+			"customer_platform_environment": customer.PlatformEnvironment,
+			"platform_environment":          platformEnvironment,
+		}).Warn("Skipping")
+		return
+	}
+
+	studioConfig, err := storageRepo.GetStudioConfig(application.TenantID)
+	if err != nil {
+		logContext.WithFields(logrus.Fields{
+			"error":       err,
+			"customer_id": application.TenantID,
+			"tip": fmt.Sprintf(
+				"didn't find a studio config for customer %s, create a config for this customer by running 'tools studio build-studio-info %s'",
+				application.TenantID,
+				application.TenantID,
+			),
+		}).Error("Failed to find studio config")
+		return
+	}
+
+	if !studioConfig.BuildOverwrite {
+		logContext.WithFields(logrus.Fields{
+			"error":       "build-overwrite-set",
+			"customer_id": application.TenantID,
+		}).Warn("Skipping")
+		return
+	}
+
+	if dryRun {
+		b, _ := json.Marshal(application)
+		fmt.Println(string(b))
+		return
+	}
+
+	err = storageRepo.SaveApplication(application)
+	if err != nil {
+		logContext.WithFields(logrus.Fields{
+			"error":     err,
+			"namespace": namespace,
+		}).Fatal("Failed to write applicaiton")
+	}
 }
 
 func init() {
 	buildApplicationInfoCMD.Flags().String("platform-environment", "dev", "Platform environment (dev or prod), not linked to application environment")
 	buildApplicationInfoCMD.Flags().Bool("dry-run", true, "Will not write to disk")
+	buildApplicationInfoCMD.Flags().Bool("all", false, "Discovers all customers from the platform and resets all studio.json's to default state (everything allowed)")
 }
--- scripts/dump-customer-tenants.sh
+++ scripts/dump-customer-tenants.sh
@@ -2,12 +2,20 @@
 #
 # 
 #
+#rm -rf /tmp/dolittle-local-dev
+#mkdir /tmp/dolittle-local-dev
+#rsync -rv --exclude='.git' ~/dolittle/git/Operations/ /tmp/dolittle-local-dev
+
+echo "Exploring"
+exit 1
 DATA=$(kubectl get ns --no-headers -o custom-columns=":metadata.name" | grep '^application-')
 DIR="/tmp/dolittle-k8s-diff"
 rm -rf $DIR
 mkdir $DIR
 
 for NAMESPACE in $DATA; do
+    echo $NAMESPACE
+    continue
     OUTPUT="${DIR}/$NAMESPACE.log"
     CMD=$(cat <<_EOF_
 
@@ -15,9 +23,10 @@ GIT_REPO_BRANCH=dev \
 GIT_REPO_DRY_RUN=true \
 GIT_REPO_DIRECTORY="/tmp/dolittle-local-dev" \
 GIT_REPO_DIRECTORY_ONLY=true \
-go run main.go tools studio build-application-info ${NAMESPACE} > ${OUTPUT}
-
+go run main.go tools studio build-terraform-info 
 _EOF_
+
+#go run main.go tools studio build-application-info ${NAMESPACE} > ${OUTPUT}
 )
     eval $CMD
 done
