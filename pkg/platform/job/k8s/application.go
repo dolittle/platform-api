48be822a8e1ac26d7507a06ad927ba38ffd1c24a
Joel Hoisko <joel.hoisko@gmail.com>
2022-02-23 15:15:38 +0100

    Fix application job to use the correct terraform template command

--- pkg/platform/job/k8s/application.go
+++ pkg/platform/job/k8s/application.go
@@ -93,7 +93,7 @@ func CreateApplicationResource(config CreateResourceConfig, customerID string, a
 							"sh",
 							"-c",
 							fmt.Sprintf(`
-/app/bin/app tools terraform create-customer-application-hcl \
+/app/bin/app tools terraform template application \
 --application-name="%s" \
 --application-id="%s" \
 --customer="%s"  > /pod-data/git/Source/V3/Azure/%s.tf;
