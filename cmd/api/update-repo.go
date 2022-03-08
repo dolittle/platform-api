7acf46d64dce772cac8ef0f2df68fbfd980764bb
freshteapot <chris@dolittle.com>
2022-03-08 11:58:47 +0100

    Create trigger file to start pull

--- cmd/api/update-repo.go
+++ cmd/api/update-repo.go
@@ -4,33 +4,33 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/dolittle/platform-api/pkg/git"
-	gitStorage "github.com/dolittle/platform-api/pkg/platform/storage/git"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var updateRepoCMD = &cobra.Command{
 	Use:   "update-repo",
 	Short: "Trigger pull on the git repo",
 	Run: func(cmd *cobra.Command, args []string) {
-		logrus.SetFormatter(&logrus.JSONFormatter{})
-		logrus.SetOutput(os.Stdout)
 
-		logContext := logrus.StandardLogger()
-		platformEnvironment := viper.GetString("tools.server.platformEnvironment")
-		gitRepoConfig := git.InitGit(logContext, platformEnvironment)
-
-		gitRepo := gitStorage.NewGitStorage(
-			logrus.WithField("context", "git-repo"),
-			gitRepoConfig,
-		)
-
-		err := gitRepo.Pull()
+		err := os.WriteFile("/tmp/trigger-git-pull", []byte(""), 0644)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
+		//platformEnvironment := viper.GetString("tools.server.platformEnvironment")
+		//gitRepoConfig := git.InitGit(logContext, platformEnvironment)
+		//
+		//os.Cre
+		//gitRepo := gitStorage.NewGitStorage(
+		//	logrus.WithField("context", "git-repo"),
+		//	gitRepoConfig,
+		//)
+		//
+		//err := gitRepo.Pull()
+		//if err != nil {
+		//	fmt.Println(err)
+		//	return
+		//}
 	},
 }
