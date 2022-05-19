0830782adb111906b06d94c5bd6b2c20c142d9b6
Joel Hoisko <joel.hoisko@gmail.com>
2022-05-19 10:00:22 +0200

    Add some tests

--- pkg/platform/microservice/m3connector/k8s/repo.go
+++ pkg/platform/microservice/m3connector/k8s/repo.go
@@ -1,23 +1,62 @@
 package k8s
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	platformK8s "github.com/dolittle/platform-api/pkg/platform/k8s"
 	"github.com/dolittle/platform-api/pkg/platform/microservice/m3connector"
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 type k8sRepo struct {
 	k8sClient    kubernetes.Interface
 	isProduction bool
+	logger       logrus.FieldLogger
 }
 
-func NewM3ConnectorRepo(k8sClient kubernetes.Interface, isProduction bool) m3connector.K8sRepo {
+func NewM3ConnectorRepo(k8sClient kubernetes.Interface, isProduction bool, logger *logrus.Logger) m3connector.K8sRepo {
 	return &k8sRepo{
 		k8sClient:    k8sClient,
 		isProduction: isProduction,
+		logger:       logger.WithField("repo", "m3-k8s-repo"),
 	}
 }
 
 func (r *k8sRepo) UpsertKafkaFiles(applicationID, environment string, kafkaFiles m3connector.KafkaFiles) error {
+	ctx := context.TODO()
+	namespace := platformK8s.GetApplicationNamespace(applicationID)
+	name := fmt.Sprintf("%s-kafka-files", environment)
+	logContext := r.logger.WithFields(logrus.Fields{
+		"application_id": applicationID,
+		"namespace":      namespace,
+		"configmap_name": name,
+		"method":         "UpsertKafkaFiles",
+	})
+	logContext.Debug("fetcing configmap")
+
+	configMap, err := r.k8sClient.CoreV1().ConfigMaps(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	bytesConfig, err := json.MarshalIndent(kafkaFiles.Config, "", "  ")
+	if err != nil {
+		return err
+	}
+	configMap.Data["config.json"] = string(bytesConfig)
+
+	configMap.Data["accessKey.pem"] = kafkaFiles.AccessKey
+	configMap.Data["certificate.pem"] = kafkaFiles.Certificate
+	configMap.Data["ca.pem"] = kafkaFiles.CertificateAuthority
+
+	_, err = r.k8sClient.CoreV1().ConfigMaps(namespace).Update(ctx, configMap, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- pkg/platform/microservice/m3connector/k8s/repo_test.go
+++ pkg/platform/microservice/m3connector/k8s/repo_test.go
@@ -1,36 +1,125 @@
 package k8s_test
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dolittle/platform-api/pkg/platform/microservice/m3connector"
+	"github.com/dolittle/platform-api/pkg/platform/microservice/m3connector/k8s"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+	logrusTest "github.com/sirupsen/logrus/hooks/test"
+
+	corev1 "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/testing"
 )
 
 var _ = Describe("Repo", func() {
+	var (
+		clientSet        *fake.Clientset
+		repo             m3connector.K8sRepo
+		logger           *logrus.Logger
+		err              error
+		applicationID    string
+		environment      string
+		kafkaFiles       m3connector.KafkaFiles
+		updatedConfigMap *corev1.ConfigMap
+		updatedConfig    m3connector.KafkaConfig
+		getError         error
+		getConfigMap     *corev1.ConfigMap
+	)
 
-	XIt("", func() {
+	BeforeEach(func() {
 
-		// getCalled := false
-		// clientSet.AddReactor("get", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
-		// 	getCalled = true
-		// 	getAction := action.(testing.GetAction)
-		// 	name := getAction.GetName()
-		// 	expectedName := fmt.Sprintf("%s-kafka-files", environment)
-		// 	Expect(name).To(Equal(expectedName))
-
-		// 	return true, nil, k8sErrors.NewNotFound(schema.ParseGroupResource("corev1.configmaps"), name)
-		// })
-		// createCalled := false
-		// clientSet.AddReactor("create", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
-		// 	createCalled = true
-		// 	createAction := action.(testing.CreateAction)
-		// 	originalObj := createAction.GetObject()
-		// 	configMap := originalObj.(*corev1.ConfigMap)
-
-		// 	Expect(configMap.Data["accessKey.pem"]).To(Equal(""))
-
-		// 	return true, configMap, nil
-		// })
-
+		clientSet = &fake.Clientset{}
+		logger, _ = logrusTest.NewNullLogger()
+		repo = k8s.NewM3ConnectorRepo(clientSet, false, logger)
+		applicationID = "fb8836a0-4fc4-437d-8f25-bb200662f327"
+		environment = "test"
+		kafkaFiles = m3connector.KafkaFiles{
+			AccessKey:            "test-key",
+			CertificateAuthority: "test-authority",
+			Certificate:          "test-certificate",
+			Config: m3connector.KafkaConfig{
+				BrokerUrl: "test-url",
+				Topics: []string{
+					"test",
+					"topic",
+					"s",
+				},
+			},
+		}
+		updatedConfig = m3connector.KafkaConfig{}
+		getConfigMap = nil
+		getError = nil
 	})
 
+	JustBeforeEach(func() {
+		clientSet.AddReactor("get", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
+			return true, getConfigMap, getError
+		})
+
+		clientSet.AddReactor("update", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
+			updateAction := action.(testing.UpdateAction)
+			updatedConfigMap = updateAction.GetObject().(*corev1.ConfigMap)
+			err := json.Unmarshal([]byte(updatedConfigMap.Data["config.json"]), &updatedConfig)
+			fmt.Println("err:", err)
+			fmt.Println("updatedconfig:", updatedConfig)
+			return true, updatedConfigMap, nil
+		})
+
+		err = repo.UpsertKafkaFiles(applicationID, environment, kafkaFiles)
+	})
+
+	When("the kafka files configmap didn't already exist", func() {
+		BeforeEach(func() {
+			getError = k8sErrors.NewNotFound(schema.ParseGroupResource("corev1.configmaps"), fmt.Sprintf("%s-kafka-files", environment))
+		})
+
+		When("everything works", func() {
+			It("should not fail", func() {
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+
+	When("Updating an already existing kafka file", func() {
+
+		BeforeEach(func() {
+			getConfigMap = &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: fmt.Sprintf("%s-kafka-files", environment),
+				},
+				Data: map[string]string{},
+			}
+		})
+
+		Describe("with all the necessary data", func() {
+			When("everything works", func() {
+				It("should not fail", func() {
+					Expect(err).To(BeNil())
+				})
+
+				It("should overwrite the config.json with the given config", func() {
+					Expect(updatedConfig).To(Equal(kafkaFiles.Config))
+				})
+
+				It("should overwrite the access key", func() {
+					Expect(updatedConfigMap.Data["accessKey.pem"]).To(Equal(kafkaFiles.AccessKey))
+				})
+				It("should overwrite the certificate", func() {
+					Expect(updatedConfigMap.Data["certificate.pem"]).To(Equal(kafkaFiles.Certificate))
+				})
+				It("should overwrite the access key", func() {
+					Expect(updatedConfigMap.Data["ca.pem"]).To(Equal(kafkaFiles.CertificateAuthority))
+				})
+			})
+		})
+	})
 })
