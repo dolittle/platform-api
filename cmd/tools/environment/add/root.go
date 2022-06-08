ae8eb4b2be38136702c90d94c697ba7dd32165ff
Joel Hoisko <joel.hoisko@gmail.com>
2022-06-08 11:35:08 +0200

    Add 'environment add tenant' command and WIP abstraction for env tenant

---
+++ cmd/tools/environment/add/root.go
@@ -0,0 +1,12 @@
+package add
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add resources to an environment",
+	Long:  ``,
+}
+
+func init() {
+}
---
+++ pkg/platform/environment/environment.go
@@ -0,0 +1,51 @@
+package environment
+
+func AddTenant(applicationID, environment, tenantID, subdomain string) {
+
+}
+
+func AddConsumerTenant(
+	applicationID,
+	microserviceID,
+	environment,
+	tenantID,
+	subdomain,
+	producerApplicationID,
+	producerEnvironment,
+	producerMicroserviceID,
+	publicStreamID,
+	partitionID,
+	producerTenantID string) error {
+	return nil
+}
+
+func AddConsumerTenantWithCustomDatabaseNameAndCopiedCustomSSL(
+	applicationID,
+	microserviceID,
+	environment,
+	tenantID,
+	subdomain,
+	producerApplicationID,
+	producerEnvironment,
+	producerMicroserviceID,
+	publicStreamID,
+	partitionID,
+	producerTenantID,
+	databaseName,
+	sourceSSL,
+	targetSSL string) error {
+
+	return nil
+}
+
+func updateIngress() {
+
+}
+
+func updateTenantsJSON() {
+
+}
+
+func updateResources() {
+
+}
