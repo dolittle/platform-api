a8f1584119886fde0f3077f1e4599ced186a568a
goeran <mail@goeran.no>
2022-06-17 20:26:42 +0200

    Use MarkFlagRequired

--- cmd/tools/microservice/copy/root.go
+++ cmd/tools/microservice/copy/root.go
@@ -15,18 +15,18 @@ func init() {
 	RootCMD.AddCommand(environmentCMD)
 
 	environmentCMD.Flags().String("application", "", "application id")
-	environmentCMD.MarkPersistentFlagRequired("application")
+	environmentCMD.MarkFlagRequired("application")
 	viper.BindPFlag("tools.studio.cfg.application", environmentCMD.Flags().Lookup("application"))
 
 	environmentCMD.Flags().String("microservice-name", "", "Name of microservice to copy environment variables from")
-	environmentCMD.MarkPersistentFlagRequired("microservice-name")
+	environmentCMD.MarkFlagRequired("microservice-name")
 	viper.BindPFlag("tools.studio.cfg.microservice-name", environmentCMD.Flags().Lookup("microservice-name"))
 
 	environmentCMD.Flags().String("from-env", "", "The environment to copy from")
-	environmentCMD.MarkPersistentFlagRequired("from-env")
+	environmentCMD.MarkFlagRequired("from-env")
 	viper.BindPFlag("tools.studio.cfg.from-env", environmentCMD.Flags().Lookup("from-env"))
 
 	environmentCMD.Flags().String("to-env", "", "they environment to copy to")
-	environmentCMD.MarkPersistentFlagRequired("to-env")
+	environmentCMD.MarkFlagRequired("to-env")
 	viper.BindPFlag("tools.studio.cfg.to-env", environmentCMD.Flags().Lookup("to-env"))
 }
